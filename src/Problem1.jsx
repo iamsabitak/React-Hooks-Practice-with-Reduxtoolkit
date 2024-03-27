5c949eeb8edd9db99adfeacbc21e0620cfcec49c
sabita <sabitakhadka091@gmail.com>
2024-03-27 10:22:45 +0545

    fixed the small issue

--- src/Problem1.jsx
+++ src/Problem1.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function Problem1() {
   const [country, setcountry] = useState(0);
+  const [city, setCity] = useState(0);
   const countries = [
     {
       name: "USA",
@@ -32,7 +33,7 @@ function Problem1() {
         })}
       </select>
       {/* 2nd dropdown  */}
-      <select value={country} onChange={(e) => setcountry(e.target.value)}>
+      <select value={city} onChange={(e) => setCity(e.target.value)}>
         {countries[country].cities.map((item, index) => {
           return (
             <option value={index} key={item.name}>
